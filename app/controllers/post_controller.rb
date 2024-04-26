ce2c5acf15ee19d6ef02c473f8af7117bd1c0e0b
AlaaSherif55 <alaa.sherif.fathi@gmail.com>
2024-04-27 00:05:15 +0300

    add lab2

--- app/controllers/post_controller.rb
+++
@@ -1,8 +0,0 @@
-class PostController < ApplicationController 
-     def data 
-        # post = Post.create({title:"lab1.1", content: "solve lab1.1.a"}) 
-        post = Post.all
-        puts "++++ #{post.inspect}"
-     end
-
-end
